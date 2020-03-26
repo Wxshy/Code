e3444457601746e2051c90ac3a337436d79da2d7
Wxshy <stormer101.sw@gmail.com>
2020-03-26 09:14:32 +0000

    added arlo

---
+++ C++/first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	//this is how to print
+	std::cout << "Hello World!\n";
+
+	//making the var
+	int score;
+	score = 1;
+	//printing the var
+	std::cout << score
+		<< "\n";
+
+}
---
+++ arlo.py
@@ -0,0 +1,6 @@
+class Employees():
+    def __init__(self):
+        self.name = 
+        self.wage = wage
+
+emp1 = Employees()
