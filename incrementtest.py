945d2b922ff41c32563cf5ab1ce73d2a675a2c2a
Wxshy <stormer101.sw@gmail.com>
2020-04-03 01:58:38 +0100

    website mostly done

--- incrementtest.py
+++ incrementtest.py
@@ -4,33 +4,36 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from functools import partial
-global num1,num2
-num1 = 0
-num2 = 0
+
 
 class Grid(GridLayout):
     def __init__(self, **kwargs):
         super(Grid, self).__init__(**kwargs)
         self.cols = 2
+        self.num1 = 0
+        self.num2 = 0
 
 
-        self.lab = Label(text=str(num1))
+        self.lab = Label(text=str(self.num1))
         self.add_widget(self.lab)
-        self.lab2 = Label(text=str(num2))
+        self.lab2 = Label(text=str(self.num2))
         self.add_widget(self.lab2)
 
         self.add1 = Button(text="NUM1")
-        self.add1.bind(on_press=partial(self.addf, 'num1'))
+        self.add1.bind(on_press=self.addnum1)
         self.add_widget(self.add1)
         self.add2 = Button(text="NUM2")
-        self.add2.bind(on_press=partial(self.addf, 'num2'))
+        self.add2.bind(on_press=self.addnum2)
         self.add_widget(self.add2)
 
-    def addf(self, instance, att):
-        global num1, num2
-        if att == 'num1':
-            num1 += 1
-            print(num1)        
+    def addnum1(self, instance):
+        self.num1 += 1
+        print(self.num1)
+        self.lab.text = str(self.num1)
+    def addnum2(self, instance):
+        self.num2 += 1
+        print(self.num2)
+        self.lab2.text = str(self.num2)     
 
 
 
